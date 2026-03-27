8697d1ec7a5862148908d452acbe30d215311a7e
Gurnelius Terer <terergurnelius@gmail.com>
2026-03-27 10:24:41 +0300

    Modify composables.

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,11 +55,15 @@ fun Greeting(name: String, modifier: Modifier) {
         Text(
             modifier = Modifier.align(Alignment.CenterHorizontally)
                 .padding(18.dp)
+                .background(color=Color.Fuchsia)
                 .clickable { },
-            text = "Having fun?"
+            text = "Having fun?",
+            color = Color.White
         )
     }
 }
+
+val Color.Companion.Fuchsia: Color get() = Color(0xFFFF00FF)
 
 @Preview(
     showBackground = true,
