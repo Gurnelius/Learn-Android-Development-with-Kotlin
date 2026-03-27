304eee52e606539e8b8c78fde798689999b57581
Gurnelius Terer <terergurnelius@gmail.com>
2026-03-27 09:38:07 +0300

    Modify composables.

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,6 +51,9 @@ fun Greeting(name: String, modifier: Modifier) {
             style = MaterialTheme.typography.headlineLarge
         )
         Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+                .padding(18.dp)
+                .clickable { },
             text = "Having fun?"
         )
     }
