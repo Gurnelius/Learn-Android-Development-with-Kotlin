4b43aa042097bbe2050946ecced6a71456b70def
Gurnelius Terer <terergurnelius@gmail.com>
2026-04-01 09:06:49 +0300

    Clean code.

--- app/src/main/java/com/example/myspecial/application/challenge/PracticeUserInput.kt
+++ app/src/main/java/com/example/myspecial/application/challenge/PracticeUserInput.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -26,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.R
 import com.example.myspecial.application.ui.theme.AppTheme
-import org.intellij.lang.annotations.JdkConstants
 
 
 @Composable
