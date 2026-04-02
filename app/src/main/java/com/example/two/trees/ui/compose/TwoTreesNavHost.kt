40fe2c54e2723644b87dd6b6c17ce70f9a0371e5
Gurnelius Terer <terergurnelius@gmail.com>
2026-04-02 09:20:06 +0300

    Add navigation.

---
+++ app/src/main/java/com/example/two/trees/ui/compose/TwoTreesNavHost.kt
@@ -0,0 +1,29 @@
+package com.example.two.trees.ui.compose
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun TwoTreesNavHost(
+    navController: NavHostController,
+    modifier: Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = "home",
+        modifier = modifier
+    ){
+        composable(route = "home") {
+            HomeScreen()
+        }
+
+        composable(route = "tours") {
+            ToursScreen()
+        }
+    }
+}
