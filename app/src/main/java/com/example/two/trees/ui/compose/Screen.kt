0e7b7f95acd3d1bfc139ce876287a8c81a107ef5
Gurnelius Terer <terergurnelius@gmail.com>
2026-04-04 00:11:41 +0300

    Navigate to product item.

--- app/src/main/java/com/example/two/trees/ui/compose/Screen.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/Screen.kt
@@ -16,6 +16,9 @@ sealed class Screen(
     data object Home: Screen("home", R.string.home_label, Icons.Filled.Home)
     data object Tours: Screen("tours", R.string.tours_label, Icons.Filled.Place)
     data object Shop: Screen("shop", R.string.shop_label, Icons.Filled.ShoppingCart)
+
+
+    data object Product: Screen("product", 0, Icons.Filled.ShoppingCart)
 }
 
 val screens = listOf(Screen.Home, Screen.Tours, Screen.Shop)
