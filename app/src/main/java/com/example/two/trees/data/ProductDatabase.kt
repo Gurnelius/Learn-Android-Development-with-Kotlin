1e0cbc16497e6c7045dd1cfa66ffd9d76440769c
Gurnelius Terer <terergurnelius@gmail.com>
2026-04-04 11:50:16 +0300

    Instantiate room database for storing products.

---
+++ app/src/main/java/com/example/two/trees/data/ProductDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.two.trees.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Product::class], version = 1, exportSchema = false)
+abstract class ProductDatabase: RoomDatabase() {
+
+    companion object {
+        @Volatile private var INSTANCE: ProductDatabase? = null
+
+        fun getDatabase(context: Context): ProductDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context = context,
+                        klass = ProductDatabase::class.java,
+                        name = "two-trees-oil.db"
+                    ).build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
